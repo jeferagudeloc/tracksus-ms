579d2d170e103f5fffa990ee242a43d41040172d
Jeferson Agudelo <jeferson.agudelo@karibu.cl>
2020-12-31 12:56:34 -0300

    [develop] first commit + config spring

---
+++ app/src/main/java/com/ven/cwt/app/TracksusApplication.java
@@ -0,0 +1,13 @@
+package com.ven.cwt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracksusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracksusApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/ven/cwt/app/TracksusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ven.cwt.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracksusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
