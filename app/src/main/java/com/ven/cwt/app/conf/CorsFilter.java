3fc95c7f279644ba58c8bf0b9be76d975bc79827
Jeferson Agudelo <jeferson.agudelo@karibu.cl>
2021-01-13 00:44:35 -0300

    [develop] Cors Filter config + Add time to token exp

--- app/src/main/java/com/ven/cwt/app/conf/CorsFilter.java
+++ app/src/main/java/com/ven/cwt/app/conf/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.ven.cwt.app.conf;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,14 +9,19 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ven.cwt.app.security.service.JWTService;
 
 public class CorsFilter implements Filter {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
 	
 	@Autowired
 	JWTService jwtService;
@@ -30,13 +36,19 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request= (HttpServletRequest) servletRequest;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "true");
         
         String headerJWT = request.getHeader("user_session");
+        
+        if (headerJWT==null) {
+        	headerJWT = getCookieValue(request.getCookies(), "user_session");
+		}
+        
+        LOGGER.info("Security Token : {}", headerJWT);
         
         
         String url = null;
@@ -62,6 +74,16 @@ public class CorsFilter implements Filter {
 
 
     }
+    
+	private String getCookieValue(Cookie[] cookies, String cookieName) {
+		System.out.println("cookies");
+		System.out.println(cookies);
+	    return Arrays.stream(cookies)
+	            .filter(c -> c.getName().equals(cookieName))
+	            .findFirst()
+	            .map(Cookie::getValue)
+	            .orElse(null);
+	}
 
     @Override
     public void destroy() {
